fbbf4bfc9f2dfb122e86d512f6642d36f492e2b9
Pavel Plesov <pavel.plesov@gmail.com>
2011-07-04 08:33:09 +0400

    attempt to add gen_event

--- apps/ertsample/src/ertsample_keeper.erl
+++ apps/ertsample/src/ertsample_keeper.erl
@@ -1,5 +1,8 @@
 -module(ertsample_keeper).
+-behaviour(gen_event).
+
 -export([emit/1, getall/0, clearall/0]).
+-export([start_link/0, init/1, handle_event/2, code_change/3]).
 -import(ertsample_keeper_chains).
 -define(LIMIT, 2).
 
@@ -48,3 +51,21 @@ getall() ->
 clearall() ->
 	put(chains, undefined),
 	put(last, undefined).
+
+start_link() ->
+	gen_server:start_link({local, ertsample_keeper}, ertsample_keeper, [], []).
+
+init(_Args) ->
+	{ok, none}.
+
+handle_event({emit, Value}, _From) ->
+	{ok, emit(Value)};
+
+handle_event({getall}, _From) ->
+	{ok, getall()};
+
+handle_event({clearall}, _From) ->
+	{ok, clearall()}.
+
+code_change(_OldVsn, _State, _Extra) ->
+	{ok, _State}.
